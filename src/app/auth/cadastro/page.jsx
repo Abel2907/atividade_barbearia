51fe5aef2072b5130f8714aa1b100343c0536433
abel maia sousa <abel.sousa217@gmail.com>
2026-05-26 00:45:00 -0300

    feat: configurando docker e organizando estrutura prisma

--- src/app/auth/cadastro/page.jsx
+++ src/app/auth/cadastro/page.jsx
@@ -38,23 +38,28 @@ export default function cadastro(){
                 {estadoAtual===2 &&
                     <section>
                         <LoginInput nameLabel="WhatsApp" id="id-tel" type="tel" placeholder="(88) 9 9999-9999" pattern="[0-9]{2} [0-9]{1} [0-9]{4}-[0-9]{4}"></LoginInput>
-                        <h4 className="font-bold">marque as opções que vocẽ tenha se interessar</h4>
-                        <label className="flex items-center gap-2 cursor-pointer">
-                            <input type="checkbox" value="corte" className="w-4 h-4" />
-                            <span>Corte de Cabelo</span>
-                        </label>
-                        <label className="flex items-center gap-2 cursor-pointer">
-                            <input type="checkbox" value="corte" className="w-4 h-4" />
-                            <span>Barba</span>
-                        </label>
-                        <label className="flex items-center gap-2 cursor-pointer">
-                            <input type="checkbox" value="corte" className="w-4 h-4" />
-                            <span>Sobrancelha</span>
-                        </label>
+                        <div className="flex justify-center gap-3 m-5 p-3 flex-col">
+                            <h4 className="font-bold">marque as opções que vocẽ tenha se interessar</h4>
+                            <label className="flex items-center gap-2 cursor-pointer">
+                                <input type="checkbox" value="corte" className="w-4 h-4" />
+                                <span>Corte de Cabelo</span>
+                            </label>
+                            <label className="flex items-center gap-2 cursor-pointer">
+                                <input type="checkbox" value="corte" className="w-4 h-4" />
+                                <span>Barba</span>
+                            </label>
+                            <label className="flex items-center gap-2 cursor-pointer">
+                                <input type="checkbox" value="corte" className="w-4 h-4" />
+                                <span>Sobrancelha</span>
+                            </label>
+                        </div>
+                        <div className="flex justify-end">
+                            <button className="rounded-2xl w-full h-12 shadow-lg flex justify-center items-cente text-orange-900 p-2">Enviar</button>
+                        </div>
                     </section>}
             </section>
-            <section className="hidden lg:flex lg:w-1/2  ml-72 items-center ">
-                <img src={'/barbearialogin.png'} className="object-cover rounded-2xl" />
+            <section className="hidden lg:flex lg:w-1/2 m-10 ml-72 items-center ">
+                    <img src={'/barbearialogin.png'} className="object-cover rounded-2xl" />
             </section>
         </main>
     )
